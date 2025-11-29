0c046dc7c27faedc65f3a4aa50c452fdf7d4b68e
sasha <savylovskij@gmail.com>
2025-11-30 00:31:34 +0200

    feat(1): add global prefix, validator, documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Express API')
+    .setDescription('Documentation API')
+    .setVersion('1.0')
+    .setContact('John Doe', 'https://express-api.com', 'example@mail.com')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
