e030f51af58b16eac665acd1252bfca199302fe3
sasha <savylovskij@gmail.com>
2025-12-27 20:29:28 +0200

    feat(5): test deploy

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Express API')
     .setDescription('Documentation API')
-    .setVersion('1.0')
+    .setVersion('1.01')
     .setContact('John Doe', 'https://express-api.com', 'example@mail.com')
     .build();
 
